877392fb60d0a685ee84fbc0810e32e8be41231c
Tri Pham Tran <tri.phamtran@maribank.com.sg>
2023-07-09 22:03:46 +0800

    host webapp with cloudfront

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as s3Deploy from 'aws-cdk-lib/aws-s3-deployment';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront'
 
 export class InfrastructureStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -12,17 +13,31 @@ export class InfrastructureStack extends cdk.Stack {
     const bucket = new s3.Bucket(this, 'WebappBucket', {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       autoDeleteObjects: true,
-      publicReadAccess: true,
-      websiteIndexDocument: 'index.html',
-      websiteErrorDocument: 'index.html',
-      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ACLS,
-      accessControl: s3.BucketAccessControl.BUCKET_OWNER_FULL_CONTROL
+      encryption: s3.BucketEncryption.S3_MANAGED,
     })
 
+    const originAccessIdentity = new cloudfront.OriginAccessIdentity(this, 'MyOriginAccessIdentity');
+
+    bucket.grantRead(originAccessIdentity);
+
+    const webDistribution = new cloudfront.CloudFrontWebDistribution(this, 'MyCloudFrontWebDistribution', {
+      originConfigs: [
+        {
+          s3OriginSource: {
+            s3BucketSource: bucket,
+            originAccessIdentity: originAccessIdentity,
+          },
+          behaviors : [ {isDefaultBehavior: true}],
+        }
+      ],
+      priceClass: cloudfront.PriceClass.PRICE_CLASS_200
+    })
 
     new s3Deploy.BucketDeployment(this, 'DeployWebsite', {
       sources: [s3Deploy.Source.asset('../webapp/build')],
       destinationBucket: bucket,
+      distribution: webDistribution,
+      distributionPaths: ['/*'],
     });
   }
 }
